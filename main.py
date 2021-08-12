eb5eb6fc1c664a4ab042c56631255f929599640a
Xhackers7 <nishaajithkj@gmail.com>
2021-08-12 20:58:14 +0530

    Implemented image generation: Generate ppm images

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ def main():
     col2 = Color(209.0 / 255, 95.0 / 255, 200.0 / 255)
 
     for y in range(HEIGHT):
-        t = y / HEIGHT
+        t = y / HEIGHT /2
         col_one = col1 * (1.0 - t) + col2 * t
         col_two = col2 * (1.0 - t) + col1 * t
         for x in range(WIDTH):
-            lightness = x / WIDTH
+            lightness = x / WIDTH/2
             col = col_one * (1.0 - lightness) + col_two * lightness
             im.set_pixel(x, y, col)
 
